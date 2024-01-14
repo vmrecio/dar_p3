795bf8cc25572bc8d01eb2d6323a426f3cf758b5
Víctor Manuel Recio Valle <victor@mini-de-victor.lan>
2024-01-15 00:03:35 +0100

    Eliminados comentarios de desarrollo

--- src/components/catalog/CatalogFilters.jsx
+++ src/components/catalog/CatalogFilters.jsx
@@ -1,32 +1,8 @@
-// import React, { useState, useEffect } from "react";
 import { useCatalog } from "../../contexts/CatalogContext";
-// import Button from "../common/Button";
 import StarRating from "../common/StarRating";
 
 const CatalogFilters = () => {
   const { catalogCatagories, filterChange } = useCatalog();
-  // const { setFilter, resetCatalog, getFilters, selectedCategories } = useCatalog();
-  // const [checkedCategories, setCheckedCategories] = useState(new Set());
-
-  // useEffect(() => {
-  //   setCheckedCategories(new Set(selectedCategories));
-  // }, [selectedCategories]);
-
-  // const handleCategoryChange = (category, isChecked) => {
-  //   if (isChecked) {
-  //     setCheckedCategories(prev => new Set(prev.add(category)));
-  //   } else {
-  //     setCheckedCategories(prev => {
-  //       const newSet = new Set(prev);
-  //       newSet.delete(category);
-  //       return newSet;
-  //     });
-  //   }
-  //   setFilter(category, isChecked);
-  // };
-
-  // const categories = Array.from(getFilters());
-
   const handleFilterChange = (filterName, value, ev) => {
     const action = ev.target?.checked ? "add" : "remove";
     filterChange(filterName, value, action);
